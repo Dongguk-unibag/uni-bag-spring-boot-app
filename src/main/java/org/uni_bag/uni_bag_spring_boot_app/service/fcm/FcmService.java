8d8c289228dffd75486208e3007866eb85fb3be4
minnnisu <mine52100@naver.com>
2025-03-11 18:22:48 +0900

    feat: FCM 메시지 전송 기능 구현

--- src/main/java/org/uni_bag/uni_bag_spring_boot_app/service/fcm/FcmService.java
+++ src/main/java/org/uni_bag/uni_bag_spring_boot_app/service/fcm/FcmService.java
@@ -1,5 +1,8 @@
 package org.uni_bag.uni_bag_spring_boot_app.service.fcm;
+
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +12,9 @@ import org.uni_bag.uni_bag_spring_boot_app.domain.User;
 import org.uni_bag.uni_bag_spring_boot_app.dto.fcm.FcmTokenSaveRequestDto;
 import org.uni_bag.uni_bag_spring_boot_app.dto.fcm.FcmTokenSaveResponseDto;
 import org.uni_bag.uni_bag_spring_boot_app.repository.FcmTokenRepository;
+
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -16,8 +22,36 @@ import org.uni_bag.uni_bag_spring_boot_app.repository.FcmTokenRepository;
 public class FcmService {
     private final FirebaseMessaging firebaseMessaging;
     private final FcmTokenRepository fcmTokenRepository;
+
+    public void sendNotification(User user, String title, String body) {
+        List<FcmToken> fcmTokens = fcmTokenRepository.findByUser(user);
+
+        fcmTokens.forEach(fcmToken -> {
+            log.info("Attempting to send Notification (title: {}, body: {}, fcmToken: {})", title, body, fcmToken.getFcmToken());
+            send(createMessage(title, body, fcmToken.getFcmToken()));
+        });
+    }
+
     public FcmTokenSaveResponseDto saveFcmToken(User user, FcmTokenSaveRequestDto fcmTokenSaveRequestDto) {
         fcmTokenRepository.save(FcmToken.of(user, fcmTokenSaveRequestDto.getFcmToken()));
         return FcmTokenSaveResponseDto.of(user.getId(), fcmTokenSaveRequestDto.getFcmToken());
+    }
+
+
+    private void send(Message message) {
+        try {
+            String response = firebaseMessaging.send(message);
+            log.info("Successfully send Notification: {}", response);
+        } catch (FirebaseMessagingException e) {
+            log.error("Fail to send Notification : {}", e.getMessage());
+        }
+    }
+
+    private Message createMessage(String title, String body, String fcmToken) {
+        return Message.builder()
+                .putData("title", title)
+                .putData("body", body)
+                .setToken(fcmToken)
+                .build();
     }
 }
