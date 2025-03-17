c9abb797d11d4337dec969034d6b639db0054088
minnnisu <mine52100@naver.com>
2025-03-17 16:06:05 +0900

    feat: 강의 검색 기능에 대해 커서 기반 페이지네이션 적용

--- src/main/java/org/uni_bag/uni_bag_spring_boot_app/controller/lectureSearch/LectureSearchController.java
+++ src/main/java/org/uni_bag/uni_bag_spring_boot_app/controller/lectureSearch/LectureSearchController.java
@@ -10,12 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.uni_bag.uni_bag_spring_boot_app.config.HttpErrorCode;
 import org.uni_bag.uni_bag_spring_boot_app.dto.lectureSearch.LectureSearchResponseDto;
-import org.uni_bag.uni_bag_spring_boot_app.dto.myTimeTable.MyTimeTableReadResponseDto;
 import org.uni_bag.uni_bag_spring_boot_app.service.lectureSearch.LectureSearchService;
-import org.uni_bag.uni_bag_spring_boot_app.swagger.ApiErrorCodeExample;
-import org.uni_bag.uni_bag_spring_boot_app.swagger.ApiErrorCodeExamples;
 import org.uni_bag.uni_bag_spring_boot_app.swagger.JwtTokenErrorExample;
 
 @RestController
@@ -31,13 +27,14 @@ public class LectureSearchController {
     @GetMapping("/{year}/{semester}")
     public ResponseEntity<LectureSearchResponseDto> searchLecture(@Parameter(example = "2024", required = true, description = "년도") @PathVariable int year,
                                                                   @Parameter(example = "3", required = true, description = "학기") @PathVariable int semester,
+                                                                  @Parameter(example = "1", description = "cursorId") @RequestParam(required = false, defaultValue = "0") Long cursorId,
                                                                   @Parameter(example = "불교문화대학", description = "개설대학") @RequestParam(required = false) String oc,
                                                                   @Parameter(example = "불교학부", description = "개설학과") @RequestParam(required = false) String od,
                                                                   @Parameter(example = "불교학전공", description = "개설전공") @RequestParam(required = false) String om,
                                                                   @Parameter(example = "1", description = "학년")    @RequestParam(required = false) Integer grade,
                                                                   @Parameter(example = "홍길동", description = "담당교수") @RequestParam(required = false) String professor,
                                                                   @Parameter(example = "강의 이름", description = "대승불교개론") @RequestParam(required = false) String lectureName) {
-        LectureSearchResponseDto responseDto = lectureSearchService.searchLecture(year, semester,oc, od, om, grade, professor, lectureName);
+        LectureSearchResponseDto responseDto = lectureSearchService.searchLecture(cursorId, year, semester,oc, od, om, grade, professor, lectureName);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 }
