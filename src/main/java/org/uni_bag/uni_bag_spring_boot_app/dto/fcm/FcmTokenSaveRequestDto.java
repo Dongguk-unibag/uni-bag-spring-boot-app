0c4f8e1800fc44e8d4f5f9ed8ea41d9169ed5f69
minnnisu <mine52100@naver.com>
2025-08-29 16:12:46 +0900

    feat: FcmControllerTest 작성

--- src/main/java/org/uni_bag/uni_bag_spring_boot_app/dto/fcm/FcmTokenSaveRequestDto.java
+++ src/main/java/org/uni_bag/uni_bag_spring_boot_app/dto/fcm/FcmTokenSaveRequestDto.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class FcmTokenSaveRequestDto {
     @Schema(example = "d6s5YYBcRbSogj9YijTalS:APA91bG8eSQ6IVTNGp6E-aD5DYw62J7k013Evn...", description = "FCM 토큰")
